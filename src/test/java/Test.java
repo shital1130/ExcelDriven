5aa7fc904563582d108378d48795adfe9f6d7086
shital1130 <shital.shewale@adnetglobal.com>
2022-09-09 16:53:02 +0530

    third commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		DataDrivenDemo d = new DataDrivenDemo();
+		ArrayList data = d.getData("Add Profile");
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+		
+	}
+}
